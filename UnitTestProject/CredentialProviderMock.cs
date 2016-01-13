c4eddf2ef6bb0e040702f206f9be78e58fa9cd36
nicklasjepsen <nicklas.m.jepsen@gmail.com>
2016-01-13 20:51:56 +0100

    Addded

---
+++ UnitTestProject/CredentialProviderMock.cs
@@ -0,0 +1,18 @@
+﻿using SystemOut.CalandarApi;
+
+namespace UnitTestProject
+{
+    public class CredentialProviderMock : ICredentialProvider
+    {
+        public string ValidId { get; set; }
+        public CalendarCredential GetCredentials(string id)
+        {
+            if (id == ValidId)
+                return new CalendarCredential
+                {
+                    Type = "ICS",
+                };
+            return null;
+        }
+    }
+}
