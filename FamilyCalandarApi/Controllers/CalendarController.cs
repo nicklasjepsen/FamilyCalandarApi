fdf3a547b597145c08110cab6a31ea2f24a51b60
nicklasjepsen <nicklas.m.jepsen@gmail.com>
2016-01-13 23:26:27 +0100

    Fixed the timezone problem.

--- FamilyCalandarApi/Controllers/CalendarController.cs
+++ FamilyCalandarApi/Controllers/CalendarController.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using Microsoft.Exchange.WebServices.Data;
 using SystemOut.Toolbox;
+using SystemOut.Toolbox.Core;
 
 namespace SystemOut.CalandarApi.Controllers
 {
@@ -32,57 +33,56 @@ namespace SystemOut.CalandarApi.Controllers
         [HttpGet]
         public CalendarModel Get(string id)
         {
-            try
-            {
-                // TODO: Implement your own credential provider
-                var credentials = credentialProvider.GetCredentials(id);
-                if (credentials == null)
-                    throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, ""));
+            var credentials = credentialProvider.GetCredentials(id);
+            if (credentials == null)
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, ""));
 
-                switch (credentials.Type)
-                {
-                    case "EWS":
-                        var ewsService = new ExchangeService
-                        {
-                            Credentials = new WebCredentials(credentials.Username, credentials.Password, credentials.Domain),
-                            Url = new Uri(credentials.ServiceUrl)
-                        };
-                        var week = ewsService.FindAppointments(WellKnownFolderName.Calendar,
-                            new CalendarView(DateTime.Today, DateTime.Today.AddDays(7)));
-
-                        return new CalendarModel
-                        {
-                            Owner = id,
-                            Appointments = week.Select(a => new AppointmentModel
-                            {
-                                Subject = a.Subject,
-                                StartTime = a.Start.ToUniversalTime(),
-                                EndTime = a.End.ToUniversalTime(),
-                                Duration = a.Duration,
-                                IsPrivate = a.Sensitivity == Sensitivity.Private || a.Sensitivity == Sensitivity.Confidential
-                            })
-                        };
-                    case "ICS":
-                        var icsCal = GetIcsCalendar(credentials.ServiceUrl);
-                        return new CalendarModel
-                        {
-                            Owner = id,
-                            Appointments = icsCal.Where(a => a != null && a.StartDate.Date >= DateTime.UtcNow.Date && a.EndDate < DateTime.UtcNow.Date.AddDays(8))
-                            .Select(e => new AppointmentModel
-                            {
-                                Subject = e.Summary,
-                                StartTime = e.StartDate,
-                                EndTime = e.EndDate,
-                            })
-                        };
-                    default: throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.BadRequest, ""));
-                }
-            }
-            catch (Exception e)
+            switch (credentials.Type)
             {
-                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message));
+                case "EWS":
+                    var ewsService = new ExchangeService
+                    {
+                        Credentials = new WebCredentials(credentials.Username, credentials.Password, credentials.Domain),
+                        Url = new Uri(credentials.ServiceUrl)
+                    };
+                    var week = ewsService.FindAppointments(WellKnownFolderName.Calendar,
+                        new CalendarView(DateTime.Today, DateTime.Today.AddDays(7)));
+
+                    return new CalendarModel
+                    {
+                        Owner = id,
+                        Appointments = week.Select(a => new AppointmentModel
+                        {
+                            Subject = a.Subject,
+                            StartTime = a.Start.ToUniversalTime(),
+                            EndTime = a.End.ToUniversalTime(),
+                            Duration = a.Duration,
+                            IsPrivate =
+                                a.Sensitivity == Sensitivity.Private || a.Sensitivity == Sensitivity.Confidential
+                        })
+                    };
+                case "ICS":
+                    var icsCal = GetIcsCalendar(credentials.ServiceUrl);
+                    return new CalendarModel
+                    {
+                        Owner = id,
+                        Appointments =
+                            icsCal.Where(
+                                a =>
+                                    a != null && a.StartDate.Date >= DateTime.UtcNow.Date &&
+                                    a.EndDate < DateTime.UtcNow.Date.AddDays(8))
+                                .Select(e => new AppointmentModel
+                                {
+                                    Subject = e.Summary,
+                                    StartTime = e.StartDate,
+                                    EndTime = e.EndDate,
+                                })
+                    };
+                default:
+                    throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.BadRequest, ""));
             }
         }
+
 
         // OData support follows
         //[EnableQuery]
@@ -155,37 +155,30 @@ namespace SystemOut.CalandarApi.Controllers
                         var value = line.Substring(splitterIndex + 1, line.Length - splitterIndex - 1);
                         if (props.ContainsKey(key))
                             continue;
+                        if (key.Contains("DTSTART;TZID=") ||
+                            key.Contains("DTEND;TZID="))
+                        {
+                            var result = ParseDateForSpecificTimezone(key, value);
+                            props.Add(result.Key, result.Value);
+                        }
                         else
                         {
-                            if (key.Contains("DTSTART;TZID="))
-                            {
-                                // Handle timezone different
-                                // First get timezone
-                                var splitted = key.Split('=');
-                                if (splitted.Length > 1)
-                                {
-                                    var tzStr = splitted[1];
-                                    var tz = Converter.
-
-                                }
-                            }
-                        props.Add(key, value);
+                            props.Add(key, value);
                         }
                     } while (allLines[y] != "END:VEVENT");
                     var vevent = new VEvent();
                     string created, summary, startTime, endTime, sequence, uid;
                     if (props.TryGetValue("CREATED", out created))
                     {
-                        // 20141110T180231Z
-                        vevent.Created = DateTime.ParseExact(created, "yyyyMMddTHHmmssZ", CultureInfo.InvariantCulture);
+                    // 20141110T180231Z
+                    vevent.Created = DateTime.ParseExact(created, "yyyyMMddTHHmmssZ", CultureInfo.InvariantCulture);
                     }
                     if (props.TryGetValue("SUMMARY", out summary))
                     {
                         vevent.Summary = summary;
                     }
-                    // TODO: Handle timezone
-                    if (props.TryGetValue())
-                    if (props.TryGetValue("DTSTART;TZID=Europe/Copenhagen", out startTime))
+                // TODO: Handle timezone
+                if (props.TryGetValue("DTSTART_PARSED", out startTime))
                     {
                         vevent.StartDate = DateTime.ParseExact(startTime, "yyyyMMddTHHmmss", CultureInfo.InvariantCulture);
                     }
@@ -193,7 +186,7 @@ namespace SystemOut.CalandarApi.Controllers
                     {
                         vevent.StartDate = DateTime.ParseExact(startTime, "yyyyMMdd", CultureInfo.InvariantCulture);
                     }
-                    if (props.TryGetValue("DTEND;TZID=Europe/Copenhagen", out endTime))
+                    if (props.TryGetValue("DTEND_PARSED", out endTime))
                     {
                         vevent.EndDate = DateTime.ParseExact(endTime, "yyyyMMddTHHmmss", CultureInfo.InvariantCulture);
                     }
@@ -214,6 +207,37 @@ namespace SystemOut.CalandarApi.Controllers
             });
 
             return events;
+        }
+
+        class DateTimeParseResult
+        {
+            public string Key { get; private set; }
+            public string Value { get; private set; }
+
+            public DateTimeParseResult(string key, string value)
+            {
+                Key = key;
+                Value = value;
+            }
+        }
+
+        private DateTimeParseResult ParseDateForSpecificTimezone(string key, string value)
+        {
+            // Handle timezone different
+            // First get timezone
+            var splitted = key.Split('=');
+            if (splitted.Length > 1)
+            {
+                var tzStr = splitted[1];
+                var tz = Converters.OlsonTimeZoneToTimeZoneInfo(tzStr);
+                var utcDt = TimeZoneInfo.ConvertTime(
+                    DateTime.ParseExact(value, "yyyyMMddTHHmmss", CultureInfo.InvariantCulture),
+                    tz, TimeZoneInfo.Utc);
+
+                return new DateTimeParseResult(key.Split(';').First() + "_PARSED", utcDt.ToString("yyyyMMddTHHmmss"));
+
+            }
+            return new DateTimeParseResult(key, value);
         }
 
         class VEvent
